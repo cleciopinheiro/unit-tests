29acc320afc0e163e5ab1a8ffc08cadebb5c5237
cleciopinheiro <cleciosantosdeveloper@gmail.com>
2023-01-23 19:51:47 -0300

    feat: requisito 6 finalizado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(productDetails).toBeInstanceOf(Function);
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('Alcool gel'))).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Alcool gel', 'Máscara')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Alcool gel', 'Máscara')).toEqual('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toEqual(productDetails('Alcool gel', 'Máscara')[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('Alcool gel', 'Máscara')[0].details.productId && productDetails('Alcool gel', 'Máscara')[1].details.productId).toContain('123');
   });
 });
