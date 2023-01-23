b970e68811dac7f64e72e6d7cb045e8b08a8a687
cleciopinheiro <cleciosantosdeveloper@gmail.com>
2023-01-23 14:36:21 -0300

    feat: requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -11,6 +11,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  if (array.length < 1) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  soma = Math.round(soma / (array.length));
+  return soma;
+};
 
 module.exports = average;
